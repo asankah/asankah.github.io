f56fadcb219fa542dd947c10758f2db4b634ccf4
Asanka Herath <asanka@google.com>
2020-07-14 15:01:54 -0400

    u

--- docs/build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname "$0")
-sh $BASEDIR/build.sh
-git commit -a -m 'formatting'
-git push
-(cd $BASEDIR/../../identity-domains/docs && sh build.sh && git commit -a -m 'formatting' && git push)
-(cd $BASEDIR/../../ephemeral-fingerprinting/docs && sh build.sh && git commit -a -m 'formatting' && git push)
-(cd $BASEDIR/../../ansible-cli-sugar/docs && sh build.sh && git commit -a -m 'formatting' && git push)
