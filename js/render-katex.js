78ad1019a6e54ce34e07f15556184527d8e5d3e5
Asanka Herath <asanka@google.com>
2021-09-21 17:18:31 -0400

    Updates

---
+++ js/render-katex.js
@@ -0,0 +1,21 @@
+function renderMath() {
+  let inlineOptions = {
+    throwOnError: false,
+    trust: true
+  };
+  let displayOptions = {
+    throwOnError: false,
+    trust: true,
+    displayMode: true
+  };
+
+  for (let e of document.getElementsByClassName("math")) {
+    if (e.classList.contains('display')) {
+      katex.render(e.textContent, e, displayOptions);
+    } else {
+      katex.render(e.textContent, e, inlineOptions);
+    }
+  }
+}
+
+document.addEventListener('readystatechange', renderMath);
